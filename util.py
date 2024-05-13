5569d88e9e3c1cdd2eb895944a7df72820aad307
cheezballs08 <gamerulzproz@gmail.com>
2024-05-13 17:30:56 +0300

    Fixed a bug regarding the singleton class

--- util.py
+++ util.py
@@ -3,6 +3,6 @@ class Singleton:
 
     @classmethod
     def get_instance(cls):
-        if cls._instances is None:
+        if cls._instance is None:
             cls._instance = cls.__new__(cls)
         return cls._instance
