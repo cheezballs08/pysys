bbe171ae02d7f37b3713532be90b6671999b07c6
cheezballs08 <gamerulzproz@gmail.com>
2024-05-13 17:57:06 +0300

    Implemented scheduler behaviour and modified other classes for ease of use

--- pysys.py
+++ pysys.py
@@ -17,8 +17,13 @@ class Subsystem:
     
     logger: Logger = Logger.get_instance()
     
-    def __init__(self, name: str):
+    current_command: "Command" = None
+    
+    default_command: "Command" = None
+    
+    def __init__(self, name: str, default_command: "Command"):
         self.name = name
+        self.default_command = default_command
         
     def periodic(self):
         self.logger.log_to_terminal(f"{self.name} has executed its periodic function.")
@@ -29,6 +34,10 @@ class Command:
     name: str = "Command"
     
     logger: Logger = Logger.get_instance()
+    
+    subsystems: list[Subsystem] = []
+    
+    is_executing: bool = False
     
     def __init__(self, name: str):
         self.name = name
@@ -47,3 +56,60 @@ class Command:
         
     def should_end(self) -> bool:
         pass
+    
+class Scheduler(Singleton):
+    
+    name: str = "Scheduler"
+    
+    logger: Logger = Logger.get_instance()
+    
+    subsystems: list[Subsystem] = []
+    
+    commands: list[Command] = []
+    
+    idle_commands: list[Command] = []
+    scheduled_commands: list[Command] = []
+    executing_commands: list[Command] = []
+    finalizing_commands: list[Command] = []
+    
+    def run_subsystem_periodics(self):
+        for subsystem in self.subsystems:
+            subsystem.periodic()
+            
+    def interrupt_command(self, command: Command):
+        if command in self.executing_commands:
+            self.executing_commands.remove(command)
+            command.is_executing = False
+            self.finalizing_commands.append(command)
+    
+    def interrupt_commands(self, commands: list[Command]):
+        for command in commands:
+            self.interrupt_command(command)
+
+    def check_command_ends(self):
+        for command in self.executing_commands:
+            if command in self.executing_commands:
+                self.interrupt_command(command)
+                
+    def check_subsystem_default_commands(self):
+        for subsystem in self.subsystems:
+            if subsystem.default_command in self.idle_commands:
+                self.schedule_command(subsystem.default_command)
+    
+    def schedule_command(self, command: Command):
+        if command in self.idle_commands:
+            for subsystem in command.subsystems:
+                if subsystem.current_command is not command:
+                    self.interrupt_command(subsystem.current_command)
+                    subsystem.current_command = command
+                    
+    def schedule_commands(self, commands: list[Command]):
+        for command in commands:
+            self.schedule_command(command)
+            
+    def finalize_commands(self):
+        for command in self.finalizing_commands:
+            command.finalize()
+            command.is_executing = False
+            self.finalizing_commands.remove(command)
+            self.idle_commands.append(command)
