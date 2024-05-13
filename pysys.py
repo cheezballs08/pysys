9bf650739186409be290d4b34ed750ca8115af85
cheezballs08 <gamerulzproz@gmail.com>
2024-05-13 17:35:15 +0300

    implemented very basic subsystem behaviour

--- pysys.py
+++ pysys.py
@@ -10,3 +10,16 @@ class Logger(Singleton):
     def log_to_file(self, message: str, file_name: str):
         with open(file_name, "a") as file:
             file.write(message)
+            
+class Subsystem:
+    
+    name: str = "Subsystem"
+    
+    logger: Logger = Logger.get_instance()
+    
+    def __init__(self, name: str):
+        self.name = name
+        
+    def periodic(self):
+        self.logger.log_to_terminal(f"{self.name} has executed its periodic function.")
+        self.logger.log_to_file(f"{self.name} has executed its periodic function.", "log.txt")
