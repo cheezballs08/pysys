bc4cf86afce83bc6f7c4abc54f07b0601ca925ec
cheezballs08 <gamerulzproz@gmail.com>
2024-05-13 17:38:20 +0300

    Implemented very basic command behaviour

--- pysys.py
+++ pysys.py
@@ -23,3 +23,27 @@ class Subsystem:
     def periodic(self):
         self.logger.log_to_terminal(f"{self.name} has executed its periodic function.")
         self.logger.log_to_file(f"{self.name} has executed its periodic function.", "log.txt")
+        
+class Command:
+    
+    name: str = "Command"
+    
+    logger: Logger = Logger.get_instance()
+    
+    def __init__(self, name: str):
+        self.name = name
+        
+    def initialize(self):
+        self.logger.log_to_terminal(f"{self.name} has initialized.")
+        self.logger.log_to_file(f"{self.name} has initialized.", "log.txt")
+        
+    def execute(self):
+        self.logger.log_to_terminal(f"{self.name} has executed.")
+        self.logger.log_to_file(f"{self.name} has executed.", "log.txt")
+        
+    def finalize(self):
+        self.logger.log_to_terminal(f"{self.name} has finalized.")
+        self.logger.log_to_file(f"{self.name} has finalized.", "log.txt")
+        
+    def should_end(self) -> bool:
+        pass
