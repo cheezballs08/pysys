d7d944bc494741bbec3dfd397007a43a34353e9f
cheezballs08 <gamerulzproz@gmail.com>
2024-05-15 13:02:53 +0300

    Added example useage in main.py file to function as a guide.

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ logger = pysys.Logger.get_instance()
 #Object definitions
 
 #User objects
+command1 = pysys.Command("Command 1")
+command2 = pysys.Command("Command 2")
+command3 = pysys.Command("Command 3")
+command4 = pysys.Command("Command 4")
 
-#Put your objects here
-
+subsystem1 = pysys.Subsystem("Subsystem 1")
+subsystem2 = pysys.Subsystem("Subsystem 2")
 #User objects
 
 #Setup
-scheduler.setup_scheduler({}, {})
+scheduler.setup_scheduler(
+    subsystem_commands_dictionary=
+    {
+        subsystem1: (command1, command2),
+        subsystem2: (command3, command4, command2)
+    },
+    subsystem_default_command_dicitionary=
+    {
+        subsystem1: command1,
+        subsystem2: command3
+    }
+    )
 
-system.setup_system(loop_period=0.5)
+system.setup_system(loop_period=0.02)
 #Setup
 
 #User setup
 
-#Set up anything you want here
+scheduler.schedule_command(command4)
 
 #User setup
 
@@ -36,8 +51,17 @@ with open("log.txt", "w") as file:
 while system.is_active:
     os.system('cls' if os.name == 'nt' else 'clear')
     
+    if system.tick_count == 5:
+        scheduler.schedule_command(command2)
+        
+    if system.tick_count == 8:
+        scheduler.interrupt_command(command4)
+        
+    if system.tick_count == 12:
+        scheduler.interrupt_command(command2)
+    
     system.run_system()
     
-    if system.tick_count == 100:
+    if system.tick_count == 17:
         system.exit_system()
 #Main Loop
